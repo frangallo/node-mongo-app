b8827799b2d2dc76e4b6b9341a6b9808b2efee02
frangallo <fgallo@me.com>
2015-10-30 10:52:10 -0700

    add hello world route/view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+route.get('/helloworld', function (req, res) {
+  res.render('helloworld' { title: 'Hello, World!'})
+});
+
 module.exports = router;
