498a6f4b14f4ee293a65fb12e3b1d0a22c9808b8
frangallo <fgallo@me.com>
2015-11-02 10:08:05 -0800

    add post route

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,24 @@ router.get('/newuser', function(req, res) {
   res.render('newuser', {title: 'Add New User'});
 });
 
+router.post('/adduser', function(req,res){
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  var collection = db.get("usercollection");
+
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function(err, doc) {
+    if (err){
+      res.send("There was a problem")
+    } else {
+      res.redirect("userlist")
+    }
+  });
+});
+
 module.exports = router;
